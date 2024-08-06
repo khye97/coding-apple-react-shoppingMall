dfa4db696324508197f1f534614f10fdb409ce83
Hyejin Kim <khyejin08@naver.com>
2024-08-06 17:25:24 +0900

    useEffect2

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -3,7 +3,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 
 let YellowBtn = styled.button`
@@ -11,24 +11,46 @@ let YellowBtn = styled.button`
   padding: 20px
 `
 
-
-
 function Detail (props){
   useEffect(() => {
-    setTimeout(()=>{
-      setTimer(false);
-    }, 1000)
-  })
-  let [timer, setTimer] = useState(true);
+    setTimeout(()=>{ setAlertTimer(false); }, 2000);
+    return () => { clearTimeout() }
+  }, []);
+
+  let [alertTimer, setAlertTimer] = useState(true);
   let {id} = useParams();
+  let [num, setNum] = useState();
+  let isMountedRef = useRef(false);
+  let [isMounted, setIsMounted] = useState(false);
 
   let result = props.shoes.filter(function(item){return item.id == id})
   console.log(result);
+  console.log(isMounted.current);
+
+  // useEffect(() => {
+  //   if (isMounted){
+  //     if (isNaN(num)) {
+  //       alert('숫자를 입력하세요');
+  //     }
+  //   } else {
+  //     setIsMounted(true);
+  //   }
+  // }, [num]);
+
+  useEffect(() => {
+    if (isMountedRef.current){
+      if (isNaN(num)) {
+        alert('숫자를 입력하세요');
+      }
+    } else {
+      isMountedRef.current = true;
+    }
+  }, [num]);
   
   return (
     <Container>
-      {timer && <div className='alert alert-warning'>2초 이내 구매시 할인</div>}
-     
+      {alertTimer && <div className='alert alert-warning'>2초 이내 구매시 할인</div>}
+      <input type='text' onChange={(event) => { setNum(event.target.value) }} />
       <Row>
         <Col md={6}>
           <img src={"https://codingapple1.github.io/shop/shoes"+ (Number(id) + 1) +".jpg"} width="100%" />
