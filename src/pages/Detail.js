d1268d7770afa381e45fd78d75c3e67c34e770fb
Hyejin Kim <khyejin08@naver.com>
2024-08-06 19:32:15 +0900

    tab ui 완성

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,4 +1,4 @@
-import {Button, Navbar, Container, Nav} from 'react-bootstrap'
+import {Button, Navbar, Container, Nav, Tab, Tabs} from 'react-bootstrap'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { useParams } from 'react-router-dom';
@@ -6,10 +6,10 @@ import styled from 'styled-components';
 import { useEffect, useRef, useState } from 'react';
 
 
-let YellowBtn = styled.button`
-  background: gold;
-  padding: 20px
-`
+// let YellowBtn = styled.button`
+//   background: gold;
+//   padding: 20px
+// `
 
 function Detail (props){
   useEffect(() => {
@@ -22,30 +22,31 @@ function Detail (props){
   let [num, setNum] = useState();
   let isMountedRef = useRef(false);
   let [isMounted, setIsMounted] = useState(false);
+  let [tab, setTab] = useState(0);
 
   let result = props.shoes.filter(function(item){return item.id == id})
   console.log(result);
   console.log(isMounted.current);
 
-  // useEffect(() => {
-  //   if (isMounted){
-  //     if (isNaN(num)) {
-  //       alert('숫자를 입력하세요');
-  //     }
-  //   } else {
-  //     setIsMounted(true);
-  //   }
-  // }, [num]);
-
   useEffect(() => {
-    if (isMountedRef.current){
+    if (isMounted){
       if (isNaN(num)) {
         alert('숫자를 입력하세요');
       }
     } else {
-      isMountedRef.current = true;
+      setIsMounted(true);
     }
   }, [num]);
+
+  // useEffect(() => {
+  //   if (isMountedRef.current){
+  //     if (isNaN(num)) {
+  //       alert('숫자를 입력하세요');
+  //     }
+  //   } else {
+  //     isMountedRef.current = true;
+  //   }
+  // }, [num]);
   
   return (
     <Container>
@@ -60,11 +61,46 @@ function Detail (props){
           <p>{result[0].content}</p>
           <p>{result[0].price}</p>
           <button className="btn btn-danger">주문하기</button> 
-          <YellowBtn>버튼</YellowBtn>
+          {/* <YellowBtn>버튼</YellowBtn> */}
         </Col>
+      </Row>
+      <Row>
+        {/* <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
+          <Tab eventKey="home" title="Home">
+            Tab content for Home
+          </Tab>
+          <Tab eventKey="profile" title="Profile">
+            Tab content for Profile
+          </Tab>
+          <Tab eventKey="contact" title="Contact">
+            Tab content for Contact
+          </Tab>
+        </Tabs> */}
+        <Nav variant="tabs"  defaultActiveKey="link0">
+          <Nav.Item>
+            <Nav.Link eventKey="link0" onClick={() => { setTab(0) }}>버튼0</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link1" onClick={() => { setTab(1) }}>버튼1</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link2" onClick={() => { setTab(2) }}>버튼2</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <TabContent tab={tab} />
       </Row>
     </Container>
   );
+}
+
+function TabContent (props){
+  if (props.tab == 0){
+    return <div>내용0</div>
+  } else if (props.tab == 1){
+    return <div>내용1</div>
+  } else if (props.tab == 2){
+    return <div>내용2</div>
+  }
 }
 
 export default Detail;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,9 +3,15 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import data from '../data.js';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Alert from 'react-bootstrap/Alert';
+import { useRef, useState } from 'react';
 
 
 function Main (props){
+  let [showAlert, setShowAlert] = useState(false);
+  let showMoreBtnCount = useRef(0);
+
   function sort (){
     let sortArray = [...data];
     sortArray.sort(function(a, b){
@@ -15,6 +21,43 @@ function Main (props){
     });    
     props.setShoes(sortArray);
   }
+
+  function getAjax (num){
+    axios.get(`https://codingapple1.github.io/shop/data${num}.json`)
+    .then((result) => {
+      let newArray = [...props.shoes, ...result.data];
+      props.setShoes(newArray);
+      setShowAlert(false);
+    })
+    .catch(() => {
+      console.log('ajax 요청 실패');
+    })
+    // fetch(`https://codingapple1.github.io/shop/data${num}.json`)
+    // .then(res => res.json())
+    // .then(result => {
+    //   console.log(result);
+    //   let newArray = [...props.shoes, ...result];
+    //   props.setShoes(newArray);
+    //   setShowAlert(false);
+    // })
+    // .catch(() => {
+    //   console.log('ajax 요청 실패(fetch)');
+    // })
+  }
+
+  function getData (){
+    showMoreBtnCount.current = showMoreBtnCount.current + 1;
+    console.log(showMoreBtnCount);
+    setShowAlert(true);
+    if (showMoreBtnCount.current == 1){
+      getAjax(2);
+    } else if (showMoreBtnCount.current == 2){
+      getAjax(3);
+    } else {
+      alert('마지막 페이지입니다.');
+    } 
+  }
+      
 
   return (
     <>
@@ -26,6 +69,8 @@ function Main (props){
             return <Item item={item} key={item.id} id={item.id}/>
           })}
         </Row>
+        {showAlert ? <Alert variant="primary">loading...</Alert> : null}
+        <button onClick={getData}>상품 더보기</button>
       </Container>
     </>
   );
